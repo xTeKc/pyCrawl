30ab22b66784dbb263081b5c3d3c7868bdd36ef7
xTeKc <bcd3v@pm.me>
2021-07-05 09:17:22 -0400

    refactor

--- crawl.py
+++ crawl.py
@@ -1,9 +1,13 @@
 import requests
 
-# url to crawl 
-url = "notAwebpage.duckduckgo.com"
-try:
-    get_response = requests.get("https://" + url)
-    print(get_response)
-except requests.exceptions.ConnectionError:
-    pass
+from list import subdomain_list
+
+def request(url):
+    # url to crawl 
+    url = "notAwebpage.duckduckgo.com"
+    try:
+        get_response = requests.get("https://" + url)
+        print(get_response)
+    except requests.exceptions.ConnectionError:
+        pass
+
